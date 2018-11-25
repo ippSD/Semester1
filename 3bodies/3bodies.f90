9b4b9c5b3a1152fbd8e49364f842caadffb66c0e
Imanol <imanol.sdelgado@alumnos.upm.es>
2018-11-25 18:36:08 +0100

    Puntos de Lagrange Incluidos.

--- 3bodies/3bodies.f90
+++ 3bodies/3bodies.f90
@@ -2,29 +2,62 @@ program $3bodies
     use functions
     use numerical_methods
     use cauchy
-    use dislin
+    use graphs
     implicit none
     
-    integer, parameter :: m = 2000, s = 3
+    logical, parameter :: DO_GRAPHS = .true.
+    double precision, parameter :: PI = acos(-1d0)
+    double precision, parameter :: PERIOD_MOON = 28d0 * 24d0 * 3600d0
+    double precision, parameter :: MU_EARTH = 3.986d5
+    double precision, parameter :: MU_MOON = 0.049d5
+    double precision, parameter :: A_MOON = (MU_EARTH * (PERIOD_MOON / (2d0 * PI)) ** 2d0) ** (1d0/3d0)
+    double precision, parameter :: V_MOON = 2d0*PI/PERIOD_MOON*A_MOON
+    double precision, parameter :: R0_EARTH = (MU_MOON) / (MU_MOON + MU_EARTH) * A_MOON
+    double precision, parameter :: V0_EARTH = (MU_MOON) / (MU_MOON + MU_EARTH) * V_MOON
+    double precision, parameter :: R0_MOON = A_MOON - R0_EARTH
+    double precision, parameter :: V0_MOON = V_MOON - V0_EARTH
+    double precision, parameter :: W_EM = V0_EARTH / R0_EARTH
+    
+    double precision, parameter :: R_L1 =  326400.67d0, V_L1 = W_EM*R_L1
+    double precision, parameter :: R_L2 =  473631.53d0, V_L2 = W_EM*R_L2
+    double precision, parameter :: R_L3 = -495443.51d0, V_L3 = W_EM*R_L3
+    double precision, parameter :: ANG_L4_DEG = atand(sqrt(3d0)*(R0_MOON+R0_EARTH)/(R0_MOON-R0_EARTH))
+    double precision, parameter :: R_L4 = (R0_MOON+R0_EARTH)*sqrt(3d0)/2d0/sind(ANG_L4_DEG)
+    double precision, parameter :: V_L4 = W_EM*R_L4
+    double precision, parameter :: ANG_L5_DEG = -ANG_L4_DEG, R_L5 = R_L4, V_L5 = W_EM*R_L5
+    
+    integer, parameter :: m = 400, s = 7
     integer, parameter :: n = s * 7
-    double precision, dimension(s) :: mu =  [ 8d1,1d0,0d0]
-    double precision, parameter :: v0m_s = 8d1/81d0, v0e_s = 1d0/81d0, v0s_s = 3d0
+    
+    double precision, dimension(s) :: mu =  0d0
+    double precision :: r0e_s = R0_EARTH, r0m_s = R0_MOON, r0s_s = 3d0
+    double precision :: v0e_s = V0_EARTH, v0m_s = V0_MOON, v0s_s = 3d0
 
-    double precision, dimension(3) :: x0e = [-1d0, 0d0 ,0d0]
-    double precision, dimension(3) :: v0e = [ 0d0,-v0e_s, 0d0]
-    double precision, dimension(3) :: x0m = [ 8d1, 0d0 ,0d0]
-    double precision, dimension(3) :: v0m = [ 0d0, v0m_s ,0d0]
-    double precision, dimension(3) :: x0s = [ 0d0, 9d0 ,0d0]
-    double precision, dimension(3) :: v0s = [ v0s_s, 0d0 ,0d0]
+    double precision, dimension(3) :: x0e = [ -R0_EARTH, 0d0, 0d0]
+    double precision, dimension(3) :: v0e = [ 0d0, -V0_EARTH, 0d0]
+    double precision, dimension(3) :: x0m = [ R0_MOON, 0d0, 0d0]
+    double precision, dimension(3) :: v0m = [ 0d0, V0_MOON ,0d0]
+    double precision, dimension(3) :: x0s_l1 = [R_L1, 0d0, 0d0]
+    double precision, dimension(3) :: v0s_l1 = [0d0, V_L1, 0d0]
+    double precision, dimension(3) :: x0s_l2 = [R_L2, 0d0, 0d0]
+    double precision, dimension(3) :: v0s_l2 = [0d0, V_L2, 0d0]
+    double precision, dimension(3) :: x0s_l3 = [R_L3, 0d0, 0d0]
+    double precision, dimension(3) :: v0s_l3 = [0d0, V_L3, 0d0]
+    double precision, dimension(3) :: xx_l4 = R_L4 * [  cosd(ANG_L4_DEG), sind(ANG_L4_DEG), 0d0]
+    double precision, dimension(3) :: vv_l4 = V_L4 * [- sind(ANG_L4_DEG), cosd(ANG_L4_DEG), 0d0]
+    double precision, dimension(3) :: xx_l5 = R_L5 * [  cosd(ANG_L5_DEG), sind(ANG_L5_DEG), 0d0]
+    double precision, dimension(3) :: vv_l5 = V_L5 * [- sind(ANG_L5_DEG), cosd(ANG_L5_DEG), 0d0]
     
     double precision :: tf, time(0:m), u(0:m,1:n), x_cg(0:m,3), v_cg(0:m,3)
     integer :: i, k
 
-    tf = 1d3
+    tf = PERIOD_MOON * 10
     time = [(tf/m*i,i=0,m)]
-    u(0,:) = [mu, x0e, x0m, x0s, v0e, v0m, v0s]
-    
-    
+    mu(1:2) = [ MU_EARTH, MU_MOON ]
+    u(0,1:s) = mu
+    u(0,1*s+1:4*s) = [x0e, x0m, x0s_l1, x0s_l2, x0s_l3, xx_l4, xx_l5]
+    u(0,4*s+1:7*s) = [v0e, v0m, v0s_l1, v0s_l2, v0s_l3, vv_l4, vv_l5]
+        
     call cauchy_problem(time, n_bodies, runge_kutta, u)
     
     
@@ -33,101 +66,59 @@ program $3bodies
         write(13,"(9F)") u(i, s+1:4*s)
     end do
     close(13)
-        
-    x_cg = (mu(1)*u(:,  s+1:  s+3) + mu(2)*u(:,  s+4:  s+6)) / sum(mu)
-    v_cg = (mu(1)*u(:,4*s+1:4*s+3) + mu(2)*u(:,4*s+4:4*s+6)) / sum(mu)
-    call qplot(u(:,s+1), u(:,s+2), m)  ! Earth
-    call qplot(u(:,s+4), u(:,s+5), m)  ! Moon
-    call qplot(u(:,s+7), u(:,s+8), m)  ! S/C
-    call qplot(time, x_cg(:,3), m)  ! Zcg
-    call qplot(time, v_cg(:,1), m)  ! VXcg
-    
-end program $3bodies
-    !time = [(i*dt,i=1,iters)]
-    !x_cg = (mu(1)*u(  n+1:  n+3,:) + mu(2)*u(  n+4:  n+6,:)) / sum(mu)
-    !v_cg = (mu(1)*u(4*n+1:4*n+3,:) + mu(2)*u(4*n+4:4*n+6,:)) / sum(mu)
-        
-    !call qplot(u(n+1,:), u(n+2,:), iters)  ! Earth
-    !call qplot(u(n+4,:), u(n+5,:), iters)  ! Moon
-    !call qplot(u(n+7,:), u(n+8,:), iters)  ! S/C
-    !call qplot(time, x_cg(3,:), iters)  ! Zcg
-    !call qplot(time, v_cg(1,:), iters)  ! VXcg
     
     
-    !call poincare_map(u(n+1  :n+3  ,:), v0e_s, iters)
-    !call poincare_map(u(n+1+3:n+3+3,:), v0m_s, iters)
-    !call poincare_map(u(n+1+6:n+3+6,:), v0s_s, iters)
-    
-    !call lagrange(mu(1:2), u(  n+1:  n+3,:), u(  n+4:  n+6,:), u(4*n+1:4*n+3,:), u(4*n+4:4*n+6,:), iters)
-    
-    !contains
-    
-    !subroutine poincare_map(u, param, m)
-    !    integer, intent(in) :: m
-    !    double precision, intent(in) :: u(3,m)
-    !    double precision, intent(in) :: param
-        
-    !    double precision, dimension(3) :: nor = [0d0,1d0,0d0]
-    !    double precision, dimension(3) :: p1, p2, r_c
-    !    double precision, allocatable :: r_pi(:,:), r_pi_2(:,:)
-    !    double precision :: lambda
-    !    integer :: i, k
-        
-    !    k = 1
-    !    allocate(r_pi(3,k), r_pi_2(3,k))
-    !    r_pi = 0
-    !    r_pi_2 = 0
-    !    do i = 1, m - 2
-    !        if (dot_product(nor, u(:,i))*dot_product(nor, u(:,i+1)) < 0d0 .and. u(1,i)>=0d0) then
-    !            p1 = u(:,i)
-    !            p2 = u(:,i+1)
-    !            lambda = -dot_product(nor,p1) / dot_product(nor, p2-p1)
-    !            r_c = p1 + lambda * (p2 - p1)
-            
-    !            deallocate(r_pi)
-    !            allocate(r_pi(3,k+1))
-            
-    !            r_pi(:,1:k) = r_pi_2
-    !            r_pi(:,k+1) = r_c
-            
-    !            deallocate(r_pi_2)
-    !            allocate(r_pi_2(3,k+1))
-            
-    !            r_pi_2 = r_pi
-    !            k = k + 1
-    !        end if
-    !    end do
-    
-    !    call QPLSCA([(param,i=1,k-1)], r_pi(1,2:k), k-1)
-    !    deallocate(r_pi, r_pi_2)
-    !end subroutine poincare_map
-    
-    !subroutine lagrange(mu, x1, x2, v1, v2, m)
-    !    integer, intent(in) :: m
-    !    double precision, intent(in) :: x1(3,m), x2(3,m), v1(3,m), v2(3,m), mu(2)
-        
-    !    double precision, dimension(3,m) :: xcg, x1_rel, x2_rel
-    !    double precision :: r(3,m), a(3,m), de(3), dm(3), alpha, w, y
-    !    integer :: i, k
-        
-    !    xcg = (mu(1) * x1 + mu(2) * x2) / sum(mu)
-    !    x1_rel = x1 - xcg
-    !    x2_rel = x2 - xcg
-    !    w = norm2(v2(:,1))/norm2(x2_rel(:,1))
-    !    alpha = 0d0
-    !    do i = 1, m
-    !        y = 2d0 * i / 3d0
-    !        if(i >= m / 2d0) then
-    !            y = y + m/3d0
-    !        end if
-    !        r(:,i) = (y - m/2) * 2d0 / m * 100d0 * [cosd(alpha), sind(alpha), 0d0]
-    !        dm = r(:,i) - 80d0 * [1d0,0d0,0d0]
-    !        de = r(:,i) - 1d0 * [-1d0,0d0,0d0]
-    !        a(:,i) = -mu(2)*dm/norm2(dm)**3d0 - mu(1)*de/norm2(de)**3d0 - w**2d0*r(:,i)
-    !    end do
-        
-    !    call QPLSCA(r(1,:), a(1,:), m)
-        
-    !end subroutine lagrange
-        
+    if (DO_GRAPHS) then
+        call plot_orbit_xy(u, 1)  ! Earth
+        call plot_orbit_xy(u, 2)  ! Moon
+        call plot_orbit_xy(u / R_L5, 3)  ! S/C L1
+        call plot_orbit_xy(u / R_L2, 4)  ! S/C L2
+        call plot_orbit_xy(u / R_L3, 5)  ! S/C L3
+        call plot_orbit_xy(u / R_L4, 6)  ! S/C L4
+        call plot_orbit_xy(u / R_L5, 7)  ! S/C L5
+    end if
 
+    contains
+    
+    subroutine plane_intersection_points(u, nor, inter_points)
+        double precision, intent(in) :: u(:,:), nor(3)  ! u(:,3)
+        double precision, allocatable, intent(out) :: inter_points(:,:)
+        
+        integer :: m
+        integer :: i, j, n
+        double precision :: r1, r2, p1(3), p2(3), lambda, p_corte(3)
+        
+        m = size(u(:,1))
+        n = 0
+        do i = 1, m - 1
+            r1 = dot_product(nor, u(i  ,:))
+            r2 = dot_product(nor, u(i+1,:))
+            if (r1*r2 < 0d0) then
+                n = n + 1
+            end if
+        end do
+        
+        allocate(inter_points(n, 3))
+        
+        j = 1
+        do i = 1, m - 1
+            r1 = dot_product(nor, u(i  ,:))
+            r2 = dot_product(nor, u(i+1,:))
+            if (r1*r2 < 0d0) then
+                ! Cambio de signo, se ha atravesado el plano.
+                ! r:  p = p1 + lambda * (p2 - p1)
+                ! pi: dot(p, normal) = 0
+                ! p_corte pertenece a ambos.
+                
+                p1 = u(i  ,:)
+                p2 = u(i+1,:)
+                lambda = -dot_product(nor, p1) / dot_product(nor, p2-p1)
+                p_corte = p1 + lambda * (p2 - p1)
+                
+                inter_points(j,:) = p_corte
+                j = j + 1
+            end if
+        end do
+    end subroutine plane_intersection_points
+
+end program $3bodies
